25d1a270e98db19b2bfac77cab40d723844fc5dc
Arnav_2021519 <arnav21519@iiitd.ac.in>
2022-11-14 14:29:40 +0530

    New commit

--- Q1a/Q1a.c
+++ Q1a/Q1a.c
@@ -6,6 +6,8 @@
 #include <pthread.h>
 #include <math.h>
 
+#define BILLION  1000000000L;
+
 void countA(){
     long long int i;
     for(i=0;i < (pow(2,32)-1);i++){
@@ -16,6 +18,7 @@ void countB(){
     long long int i;
     for(i=0; i < (pow(2,32)-1);i++){
     }
+
 }
 
 void countC(){
@@ -25,12 +28,48 @@ void countC(){
 }
 
 int main(){
+    struct timespec startA, stopA;
+    struct timespec startB, stopB;
+    struct timespec startC, stopC;
     pthread_t thrA, thrB, thrC;
-    pthread_create(&thrA, NULL, (void *)countA, NULL);
-    pthread_create(&thrB, NULL, (void *)countB, NULL);
-    pthread_create(&thrC, NULL, (void *)countC, NULL);
+    double resA;
+    double resB;
+    double resC;
+    pthread_create(&thrA, NULL, (void *)&countA, NULL);
+    if( clock_gettime( CLOCK_REALTIME, &startA) == -1 ) {
+      perror( "clock gettime" );
+      exit( EXIT_FAILURE );
+    }
+    pthread_create(&thrB, NULL, (void *)&countB, NULL);
+    if( clock_gettime( CLOCK_REALTIME, &startB) == -1 ) {
+      perror( "clock gettime" );
+      exit( EXIT_FAILURE );
+    }
+    pthread_create(&thrC, NULL, (void *)&countC, NULL);
+    if( clock_gettime( CLOCK_REALTIME, &startC) == -1 ) {
+      perror( "clock gettime" );
+      exit( EXIT_FAILURE );
+    }
     pthread_join(thrA, NULL);
+    if( clock_gettime( CLOCK_REALTIME, &stopA) == -1 ) {
+      perror( "clock gettime" );
+      exit( EXIT_FAILURE );
+    }
     pthread_join(thrB, NULL);
+    if( clock_gettime( CLOCK_REALTIME, &stopB) == -1 ) {
+      perror( "clock gettime" );
+      exit( EXIT_FAILURE );
+    }
     pthread_join(thrC, NULL);
+    if( clock_gettime( CLOCK_REALTIME, &stopC) == -1 ) {
+      perror( "clock gettime" );
+      exit( EXIT_FAILURE );
+    }
+    resA = ( stopA.tv_nsec - startA.tv_nsec );
+    resB = ( stopB.tv_nsec - startB.tv_nsec );
+    resC = ( stopC.tv_nsec - startC.tv_nsec );
+    printf("Thread A: %f\n", resA);
+    printf("Thread B: %f\n", resB);
+    printf("Thread C: %f\n", resC);
     return 0;
 }
