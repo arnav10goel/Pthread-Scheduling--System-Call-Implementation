a05067123a70eeda68a4a6651360bb7f759df390
Arnav_2021519 <arnav21519@iiitd.ac.in>
2022-11-22 05:08:13 +0530

    Changes to 1a and 1b

--- 2021519_Q1/2021519_Q1a/Q1a.c
+++ 2021519_Q1/2021519_Q1a/Q1a.c
@@ -23,10 +23,10 @@ void countA(int priority){
     long long int n = pow(2,32);
     for(i=1; i <= n;i++){
     }
-    if( clock_gettime( CLOCK_REALTIME, &stopA) == -1 ) {
-      perror( "clock gettime" );
-      exit( EXIT_FAILURE );
-    }
+    // if( clock_gettime( CLOCK_REALTIME, &stopA) == -1 ) {
+    //   perror( "clock gettime" );
+    //   exit( EXIT_FAILURE );
+    // }
     struct sched_param param1;
     int policy1;
     int rc = pthread_getschedparam(pthread_self(), &policy1, &param1);
@@ -47,10 +47,10 @@ void countB(int priority){
     long long int n = pow(2,32);
     for(i=1; i <= n;i++){
     }
-    if( clock_gettime( CLOCK_REALTIME, &stopB) == -1 ) {
-      perror( "clock gettime" );
-      exit( EXIT_FAILURE );
-    }
+    // if( clock_gettime( CLOCK_REALTIME, &stopB) == -1 ) {
+    //   perror( "clock gettime" );
+    //   exit( EXIT_FAILURE );
+    // }
     struct sched_param param1;
     int policy1;
     int rc = pthread_getschedparam(pthread_self(), &policy1, &param1);
@@ -70,10 +70,10 @@ void countC(int priority){
     long long int n = pow(2,32);
     for(i=1; i <= n;i++){
     }
-    if( clock_gettime( CLOCK_REALTIME, &stopC) == -1 ) {
-      perror( "clock gettime" );
-      exit( EXIT_FAILURE );
-    }
+    // if( clock_gettime( CLOCK_REALTIME, &stopC) == -1 ) {
+    //   perror( "clock gettime" );
+    //   exit( EXIT_FAILURE );
+    // }
     struct sched_param param1;
     int policy1;
     int rc = pthread_getschedparam(pthread_self(), &policy1, &param1);
@@ -85,43 +85,58 @@ int main(){
     int priority_a, priority_b, priority_c;
     printf("Enter nice value for Thread A - SCHED_OTHER (value b/w -20 and 19): ");
     scanf("%d", &priority_a);
-    printf("Enter priority for Thread B - SCHED_FIFO (value b/w 0 and 99): ");
+    printf("Enter priority for Thread B - SCHED_FIFO (value b/w 1 and 99): ");
     scanf("%d", &priority_b);
-    printf("Enter priority for Thread C - SCHED_RR (value b/w 0 and 99): ");
+    printf("Enter priority for Thread C - SCHED_RR (value b/w 1 and 99): ");
     scanf("%d", &priority_c);
 
-    pthread_t thrA, thrB, thrC;
-    double resA;
-    double resB;
-    double resC;
+    if(priority_a < -20 || priority_a > 19){
+        printf("Invalid nice value for Thread A. Exiting...");
+        exit(0);
+    }
+    else if(priority_b < 1 || priority_b > 99){
+        printf("Invalid priority for Thread B. Exiting...");
+        exit(0);
+    }
+    else if(priority_c < 1 || priority_c > 99){
+        printf("Invalid priority for Thread C. Exiting...");
+        exit(0);
+    }
+    else{
+        pthread_t thrA, thrB, thrC;
+        double resA;
+        double resB;
+        double resC;
 
-    pthread_create(&thrA, NULL, (void *)&countA, (void*)priority_a);
-    pthread_create(&thrB, NULL, (void *)&countB, (void*)priority_b);
-    pthread_create(&thrC, NULL, (void *)&countC, (void*)priority_c);
+        pthread_create(&thrA, NULL, (void *)&countA, (void*)priority_a);
+        pthread_create(&thrB, NULL, (void *)&countB, (void*)priority_b);
+        pthread_create(&thrC, NULL, (void *)&countC, (void*)priority_c);
+        
+        pthread_join(thrA, NULL);
+        if( clock_gettime( CLOCK_REALTIME, &stopA) == -1 ) {
+          perror( "clock gettime" );
+          exit( EXIT_FAILURE );
+        }
+
+        pthread_join(thrB, NULL);
+        if( clock_gettime( CLOCK_REALTIME, &stopB) == -1 ) {
+          perror( "clock gettime" );
+          exit( EXIT_FAILURE );
+        }
+
+        pthread_join(thrC, NULL);
+        if( clock_gettime( CLOCK_REALTIME, &stopC) == -1 ) {
+          perror( "clock gettime" );
+          exit( EXIT_FAILURE );
+        }
+        resA = (stopA.tv_sec - startA.tv_sec) + ( stopA.tv_nsec - startA.tv_nsec ) / 1000000000.0;
+        resB = (stopB.tv_sec - startB.tv_sec) + ( stopB.tv_nsec - startB.tv_nsec ) / 1000000000.0;
+        resC = (stopC.tv_sec - startC.tv_sec) + ( stopC.tv_nsec - startC.tv_nsec ) / 1000000000.0;
+        
+        printf("Thread A (SCHED_OTHER): %f\n", resA);
+        printf("Thread B (SCHED_FIFO): %f\n", resB);
+        printf("Thread C (SCHED_RR): %f\n", resC);
+        return 0;
+    }
     
-    pthread_join(thrA, NULL);
-    // if( clock_gettime( CLOCK_REALTIME, &stopA) == -1 ) {
-    //   perror( "clock gettime" );
-    //   exit( EXIT_FAILURE );
-    // }
-
-    pthread_join(thrB, NULL);
-    // if( clock_gettime( CLOCK_REALTIME, &stopB) == -1 ) {
-    //   perror( "clock gettime" );
-    //   exit( EXIT_FAILURE );
-    // }
-
-    pthread_join(thrC, NULL);
-    // if( clock_gettime( CLOCK_REALTIME, &stopC) == -1 ) {
-    //   perror( "clock gettime" );
-    //   exit( EXIT_FAILURE );
-    // }
-    resA = (stopA.tv_sec - startA.tv_sec) + ( stopA.tv_nsec - startA.tv_nsec ) / 1000000000.0;
-    resB = (stopB.tv_sec - startB.tv_sec) + ( stopB.tv_nsec - startB.tv_nsec ) / 1000000000.0;
-    resC = (stopC.tv_sec - startC.tv_sec) + ( stopC.tv_nsec - startC.tv_nsec ) / 1000000000.0;
-    
-    printf("Thread A (SCHED_OTHER): %f\n", resA);
-    printf("Thread B (SCHED_FIFO): %f\n", resB);
-    printf("Thread C (SCHED_RR): %f\n", resC);
-    return 0;
 }
--- 2021519_Q1/2021519_Q1b/Q1b.c
+++ 2021519_Q1/2021519_Q1b/Q1b.c
@@ -5,7 +5,6 @@
 #include <sys/types.h>
 #include <time.h>
 #include <math.h>
-#include <pthread.h>
 #include <sched.h>
 
 int main(int argc, char **argv) {
@@ -13,45 +12,78 @@ int main(int argc, char **argv) {
     int policy[3] = {SCHED_OTHER, SCHED_FIFO, SCHED_RR};
     char* paths[3] = {"script1.sh", "script2.sh", "script3.sh"};
     pid_t children[3];
-
-    for (int i = 0; i < 3; i++ ) {
-        if( clock_gettime( CLOCK_REALTIME, &start[i]) == -1 ) {
-            perror( "clock gettime" );
-            exit( EXIT_FAILURE );
+    struct sched_param param_fifo, param_rr;
+    
+    if( clock_gettime( CLOCK_REALTIME, &start[0]) == -1 ) {
+        perror( "clock gettime" );
+        exit( EXIT_FAILURE );
+    }
+    children[0] = fork();
+    if(children[0] == 0){
+        int ret = nice(10);
+        if(ret == -1){
+            perror("nice");
+            exit(EXIT_FAILURE);
         }
-        children[i] = fork();
-        if(children[i] == 0){
-            if(i == 0){
-                int ret = nice(10);
-                if(ret == -1){
-                    perror("nice");
-                    exit(EXIT_FAILURE);
-                }
-                else{
-                    printf("Nice value of Child Process %d (SCHED_OTHER) is: %d", (i+1), ret);
-                }
-            }
-            else{
-                struct sched_param param;
-                param.sched_priority = 10;
-                int ret = sched_setscheduler(0, policy[i], &param);
-                if(ret == -1){
-                    perror("sched_setscheduler");
-                    exit(EXIT_FAILURE);
-                }
-                else{
-                    printf("Policy of Child Process %d is: %d", (i+1), policy[i]);
-                    printf("Priority of Child Process %d is: %d", (i+1), param.sched_priority);
-                }
-            }
-            long long int n = (pow(2,32)-1);
-            for(long long int i = 1; i <= n;i++){
-            }
-            if(execl("/bin/ls", "ls", NULL) == -1){
-                printf("Exec call failed\n");
-            }
+        else{
+            printf("Nice value of Child Process 1 (SCHED_OTHER) is: %d", ret);
+        }
+        long long int n = (pow(2,32)-1);
+        for(long long int i = 1; i <= n;i++){
+        }
+        if(execl("/bin/ls", "ls", NULL) == -1){
+            printf("Exec call failed\n");
         }
     }
+
+    if( clock_gettime( CLOCK_REALTIME, &start[0]) == -1 ) {
+        perror( "clock gettime" );
+        exit( EXIT_FAILURE );
+    }
+    children[1] = fork();
+    if(children[1] == 0){
+        param_fifo.sched_priority = 10;
+        int ret = sched_setscheduler(getpid(), policy[1], &param_fifo);
+        if(ret == -1){
+            perror("sched_setscheduler");
+            exit(EXIT_FAILURE);
+        }
+        else{
+            printf("Policy of Child Process 2 is: %d", policy[1]);
+            printf("Priority of Child Process 2 is: %d", param_fifo.sched_priority);
+        }
+        long long int n = (pow(2,32)-1);
+        for(long long int i = 1; i <= n;i++){
+        }
+        if(execl("/bin/ls", "ls", NULL) == -1){
+            printf("Exec call failed\n");
+        }
+    }
+
+    if( clock_gettime( CLOCK_REALTIME, &start[2]) == -1 ) {
+        perror( "clock gettime" );
+        exit( EXIT_FAILURE );
+    }
+    children[2] = fork();
+    if(children[2] == 0){
+        param_rr.sched_priority = 10;
+        int ret = sched_setscheduler(getpid(), policy[2], &param_rr);
+        if(ret == -1){
+            perror("sched_setscheduler");
+            exit(EXIT_FAILURE);
+        }
+        else{
+            printf("Policy of Child Process 3 is: %d", policy[2]);
+            printf("Priority of Child Process 3 is: %d", param_rr.sched_priority);
+        }
+        long long int n = (pow(2,32)-1);
+        for(long long int i = 1; i <= n;i++){
+        }
+        if(execl("/bin/ls", "ls", NULL) == -1){
+            printf("Exec call failed\n");
+        }
+    }
+
     int* test = malloc(sizeof(int));
     for (int i = 0; i < 3; i++ ) {   //Using waitpid() to wait for a particular child process and 
         waitpid(children[i], test, 0);
@@ -67,7 +99,7 @@ int main(int argc, char **argv) {
     time_FIFO = ( stop[1].tv_sec - start[1].tv_sec ) + ( stop[1].tv_nsec - start[1].tv_nsec ) / 1000000000.0;
     time_RR = ( stop[2].tv_sec - start[2].tv_sec ) + ( stop[2].tv_nsec - start[2].tv_nsec ) / 1000000000.0;
 
-    printf("Time taken by SCHED_OTHER: %f\n", time_OTHER);
-    printf("Time taken by SCHED_FIFO: %f\n", time_FIFO);
-    printf("Time taken by SCHED_RR: %f\n", time_RR);
+    printf("Time taken by Child Process 1 - SCHED_OTHER: %f\n", time_OTHER);
+    printf("Time taken by Child Process 2 - SCHED_FIFO: %f\n", time_FIFO);
+    printf("Time taken by Child Process 3 - SCHED_RR: %f\n", time_RR);
 }
